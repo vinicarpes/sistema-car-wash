97b4683a6bda12921746b1d8dbd41acd15e0644d
Vinicius Carpes de Freitas <147566186+vinicarpes@users.noreply.github.com>
2025-04-22 16:01:20 -0300

    Add files via upload

---
+++ src/main/java/br/edu/ifsc/fln/sistema-carwash/model/domain/OrdemServico.java
@@ -0,0 +1,91 @@
+package domain;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrdemServico {
+    private long numero;
+    private double total;
+    private LocalDate agenda;
+    private double desconto = 0.0;
+    private List<ItemOS> itensOS = new ArrayList<>();
+    private Veiculo veiculo;
+
+    public OrdemServico(long numero, double total, LocalDate agenda){
+        this.numero = numero;
+        this.total = total;
+        this.agenda = agenda;
+    }
+
+    public long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public LocalDate getAgenda() {
+        return agenda;
+    }
+
+    public void setAgenda(LocalDate agenda) {
+        this.agenda = agenda;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
+    }
+
+    public double calcularServico(){
+        total =0;
+        for(ItemOS itemOS: itensOS){
+            total += itemOS.getValorServico();
+        }
+        total -=desconto;
+
+        return total;
+    }
+
+    public void add(ItemOS itemOS){
+        itensOS.add(itemOS);
+        itemOS.setOrdemServico(this);
+    }
+
+    public void atualizarPontuacaoCliente(){
+        for (ItemOS itemOS: itensOS){
+        veiculo.getCliente().getPontuacao().adicionar(itemOS.getServico().getPontos());
+        }
+    }
+
+    public void remove(ItemOS itemOS){
+        itensOS.remove(itemOS);
+        itemOS.setOrdemServico(null);
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public List<ItemOS> getItensOS() {
+        return itensOS;
+    }
+
+}
---
+++ src/main/java/br/edu/ifsc/fln/sistema-carwash/model/domain/Veiculo.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+public class Veiculo {
+
+	private int id;
+	private String placa;
+	private Modelo modelo;
+	private String observacoes;
+	private Cor cor;
+	private Cliente cliente;
+
+	public Veiculo(int id, String placa, Modelo modelo, String observacoes, Cor cor) {
+		this.id = id;
+		this.placa = placa;
+		this.modelo = modelo;
+		this. observacoes = observacoes;
+		this.cor = cor;
+	}
+	public Veiculo(int id, String placa, Modelo modelo) {
+		this.id = id;
+		this.placa = placa;
+		this.modelo = modelo;
+	}
+
+	public Veiculo(String placa, Modelo modelo){
+		this.modelo = modelo;
+		this.placa = placa;
+	}
+
+	public Veiculo(String placa){
+		this.placa = placa;
+	}
+
+	public Veiculo(){}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	public Modelo getModelo() {
+		return modelo;
+	}
+	public void setModelo(Modelo modelo) {
+		this.modelo = modelo;
+	}
+
+	public Cor getCor() {
+		return cor;
+	}
+
+	public void setCor(Cor cor) {
+		this.cor = cor;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public String toString() {
+
+		return "\nIdentificador do veículo - " + id + "\nPlaca do veículo - " + placa + "\n" + modelo + cor;
+
+	}
+
+
+}
