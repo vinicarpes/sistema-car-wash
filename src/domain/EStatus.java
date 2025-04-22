526725ea5f48816c243ad6f00ae927daa43b7900
Vinicius Carpes de Freitas <147566186+vinicarpes@users.noreply.github.com>
2025-04-22 16:09:31 -0300

    Delete src directory

--- src/domain/EStatus.java
+++
@@ -1,5 +0,0 @@
-package domain;
-
-public enum EStatus {
-    ABERTA, FECHADA, CANCELADA
-}
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module br.edu.ifsc.fln.sistemacarwash {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens br.edu.ifsc.fln.sistemacarwash to javafx.fxml;
-    exports br.edu.ifsc.fln.sistemacarwash;
-}
